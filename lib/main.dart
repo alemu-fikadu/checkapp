d6dc25f2a121cfb9d08deb41626e493c2e33cf57
Alemu fikadu <falemu19@gmail.com>
2022-03-17 17:01:09 +0300

    check

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(7),
             child: Card(
-              color: Color.fromARGB(255, 255, 255, 255),
+              color: const Color.fromARGB(255, 255, 255, 255),
               shape: RoundedRectangleBorder(
-                side: BorderSide(
+                side: const BorderSide(
                     color: Color.fromARGB(179, 0, 110, 255), width: 1),
                 borderRadius: BorderRadius.circular(10),
               ),
@@ -46,8 +46,9 @@ class MyApp extends StatelessWidget {
                   style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
                 ),
                 Card(
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
-                  color: Color.fromARGB(221, 240, 240, 240),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+                  color: const Color.fromARGB(221, 240, 240, 240),
                   child: Padding(
                     padding: const EdgeInsets.all(7),
                     child: Row(
@@ -206,7 +207,7 @@ class MyApp extends StatelessWidget {
                 const SizedBox(
                   height: 5,
                 ),
-                Text(
+                const Text(
                   'Result',
                   style: TextStyle(
                     fontSize: 13,
@@ -219,7 +220,7 @@ class MyApp extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(7),
                     child: Row(
-                      children: <Widget>[
+                      children: const <Widget>[
                         Text(
                           'RESULT',
                           style: TextStyle(
@@ -231,7 +232,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 30,
                   width: 200,
                   child: Divider(
@@ -245,7 +246,7 @@ class MyApp extends StatelessWidget {
                     padding: const EdgeInsets.all(13),
                     child: FittedBox(
                       child: Row(
-                        children: <Widget>[
+                        children: const <Widget>[
                           Text(
                             'Get Full Certification',
                             style: TextStyle(
